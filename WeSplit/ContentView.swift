ae72d411f04dfee7f4bde86dbbd6523bd52d9dfd
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-09 22:13:20 +0300

    Initial commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var checkAmount = 0.0
+  @State private var numberOfPeople = 2
+  @State private var tipPersentage = 20
+  let tipPersentages = [5, 10, 15, 20, 25, 0]
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField("Amount",
+                    value: $checkAmount,
+                    format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            .keyboardType(.decimalPad)
+          Picker("Number of people", selection: $numberOfPeople) {
+            ForEach(2..<100) {
+              Text("\($0) people")
+            }
+          }
+        }
+        Section {
+          Text(checkAmount,
+               format: .currency(code: Locale.current.currencyCode ?? "USD"))
+        }
+      }
+      .navigationTitle("WeSplit")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
